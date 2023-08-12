66f3a0e7274123c1eaef6cf5b42a7111b297ca7a
PK <pksingh_1987@yahoo.com>
2023-08-12 15:42:30 +0530

    added parsing code

---
+++ go.mod
@@ -0,0 +1,5 @@
+module updserver
+
+go 1.21.0
+
+require github.com/janstuemmel/csgo-log v1.1.1
--- server.go
+++ server.go
@@ -14,10 +14,14 @@ import (
 	"net"
 	"os"
 	"strconv"
+	"time"
+
+	csgolog "github.com/janstuemmel/csgo-log"
 )
 
 func main() {
 	serverPort := 5001
+
 	if len(os.Args) > 1 {
 		if v, err := strconv.Atoi(os.Args[1]); err != nil {
 			fmt.Printf("Invalid port %v, err %v", os.Args[1], err)
@@ -47,13 +51,63 @@ func main() {
 		}
 
 		msg := p[:nn]
-		fmt.Printf("Received %v %s\n", raddr, msg)
+		fmt.Printf("Received %v : %s\n", raddr, msg)
 
-		go func(conn *net.UDPConn, raddr *net.UDPAddr, msg []byte) {
-			_, err := conn.WriteToUDP([]byte(fmt.Sprintf("Pong: %s", msg)), raddr)
-			if err != nil {
-				fmt.Printf("Response err %v", err)
-			}
-		}(server, raddr, msg)
+		go response(server, raddr, msg)
+	}
+}
+
+func response(udpServer net.PacketConn, addr net.Addr, buf []byte) {
+	line := string(buf)
+	data := parse(line)
+	data = data + "\n"
+	writeToFile(data)
+	udpServer.WriteTo(cleanByteData([]byte(data)), addr)
+}
+
+func parse(line string) string {
+	var msg csgolog.Message
+	msg, err := csgolog.Parse(line)
+	if err != nil {
+		fmt.Println(err)
+		return "Invalid data"
+	} else {
+		// get json non-htmlescaped
+		jsn := csgolog.ToJSON(msg)
+		fmt.Println(jsn)
+		return jsn
+	}
+}
+
+func cleanByteData(input []byte) []byte {
+	cleanedData := make([]byte, 0, len(input))
+	for _, b := range input {
+		if b != 0 {
+			cleanedData = append(cleanedData, b)
+		}
+	}
+	return cleanedData
+}
+
+func writeToFile(content string) {
+	currentDate := time.Now()
+	dateString := currentDate.Format("2006-01-02") // Format: YYYY-MM-DD
+	// currentDate := time.Now().Format("2023-08-01") // Format: YYYY-MM-DD
+
+	// Create the file name with today's date
+	fileName := dateString + "-log.log"
+	// Open the file in append mode
+	file, err := os.OpenFile(fileName, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	// Append the content to the file
+	_, err = file.WriteString(content)
+	if err != nil {
+		fmt.Println("Error appending content:", err)
+		return
 	}
 }
